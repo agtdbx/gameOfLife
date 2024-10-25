0fae0442e6d86852b1d4be785de60d0044876071
agtdbx <gugus@PC-Auguste>
2024-10-25 23:32:08 +0200

    first commity

---
+++ grid.py
@@ -0,0 +1,84 @@
+import pygame as pg
+
+from pygame.math import Vector2 as vec2
+from define import  NB_W_TILE, NB_H_TILE,\
+                    MIN_TILE_SIZE, START_TILE_SIZE, MAX_TILE_SIZE,\
+                    TILE_FILL_COLOR, TILE_EMPTY_COLOR, \
+                    WIN_W, WIN_H
+
+class    Grid:
+    def __init__(self) -> None:
+        self.tiles = []
+        for _ in range(NB_H_TILE):
+            self.tiles.append([0] * NB_W_TILE)
+
+        self.drawTileBorder = True
+        self.tileSize = START_TILE_SIZE
+
+        self.offsetX = 0
+        self.offsetY = 0
+
+
+    def moveOffset(self, dir: str):
+        if dir == 'u':
+            self.offsetY + 1
+        elif dir == 'd':
+            self.offsetY - 1
+        elif dir == 'l':
+            self.offsetX + 1
+        elif dir == 'r':
+            self.offsetX - 1
+
+        self.offsetX %= NB_W_TILE
+        self.offsetY %= NB_H_TILE
+
+
+    def zoomIn(self):
+        self.tileSize = max(MIN_TILE_SIZE, self.tileSize - 5)
+
+
+    def zoomOut(self):
+        self.tileSize = min(MAX_TILE_SIZE, self.tileSize + 5)
+
+
+    def switchDrawTileBorder(self):
+        self.drawTileBorder = not self.drawTileBorder
+
+    def set(self, x: int, y: int, value: int):
+        x %= NB_W_TILE
+        y %= NB_H_TILE
+
+        self.tiles[y][x] = value
+
+
+    def get(self, x: int, y: int) -> int:
+        x %= NB_W_TILE
+        y %= NB_H_TILE
+
+        return self.tiles[y][x]
+
+
+    def draw(self, win: pg.Surface):
+        colors = [TILE_EMPTY_COLOR, TILE_FILL_COLOR]
+
+        for y in range(NB_H_TILE):
+            drawY = y * self.tileSize
+            drawRY = (y + 1) * self.tileSize
+            for x in range(NB_W_TILE):
+                drawX = x * self.tileSize
+                if drawX > WIN_W:
+                    break
+
+                drawRX = (x + 1) * self.tileSize
+                drawRect = (drawX, drawY, self.tileSize, self.tileSize)
+
+                tileStatus = self.get(x + self.offsetX, y + self.offsetY)
+
+                pg.draw.rect(win, colors[tileStatus], drawRect)
+                if self.drawTileBorder:
+                    tileStatus = not tileStatus
+                    pg.draw.line(win, colors[tileStatus], (drawX, drawY), (drawRX, drawY))
+                    pg.draw.line(win, colors[tileStatus], (drawX, drawY), (drawX, drawRY))
+
+            if drawY > WIN_H:
+                break
---
+++ main.py
@@ -0,0 +1,159 @@
+import pygame as pg
+import time
+import sys
+
+from pygame.math import Vector2 as vec2
+from define import WIN_W, WIN_H
+from grid import Grid
+
+class Game:
+    def __init__(self):
+        """
+        This method define all variables needed by the program
+        """
+        # Start of pygame
+        pg.init()
+
+        # We remove the toolbar of the window's height
+        self.winSize = vec2(WIN_W, WIN_H)
+        # We create the window
+        self.win = pg.display.set_mode(self.winSize, pg.RESIZABLE)
+
+        self.clock = pg.time.Clock() # The clock be used to limit our fps
+        self.fps = 0
+
+        self.last = time.time()
+
+        self.onFocus = True
+
+        self.runMainLoop = True
+
+        self.grid = Grid()
+
+
+    def run(self):
+        """
+        This method is the main loop of the game
+        """
+        # Game loop
+        while self.runMainLoop:
+            self.input()
+            self.tick()
+            self.render()
+            self.clock.tick(self.fps)
+
+
+    def input(self):
+        """
+        The method catch user's inputs, as key presse or a mouse click
+        """
+        # We check each event
+        self.mouseScrollState = 0
+        self.keyDown = []
+
+        for event in pg.event.get():
+            # If the event it a click on the top right cross, we quit the game
+            if event.type == pg.QUIT:
+                self.quit()
+
+            elif event.type == pg.WINDOWSIZECHANGED:
+                self.winSize = vec2(self.win.get_size())
+
+            elif event.type == pg.WINDOWFOCUSGAINED:
+                self.onFocus = True
+
+            elif event.type == pg.WINDOWFOCUSLOST:
+                self.onFocus = False
+
+            elif event.type == pg.MOUSEWHEEL:
+                self.mouseScrollState = event.y
+
+            elif event.type == pg.KEYDOWN:
+                self.keyDown.append(event.key)
+
+        self.keyboardState = pg.key.get_pressed()
+        self.mouseState = pg.mouse.get_pressed()
+        self.mousePos = pg.mouse.get_pos()
+
+        # Press espace to quit
+        if self.keyboardState[pg.K_ESCAPE]:
+            self.quit()
+
+
+    def tick(self):
+        """
+        This is the method where all calculations will be done
+        """
+        tmp = time.time()
+        delta = tmp - self.last
+        self.last = tmp
+
+        # Zoom management
+        if self.mouseScrollState == 1:
+            self.grid.zoomOut()
+        elif self.mouseScrollState == -1:
+            self.grid.zoomIn()
+
+        # Modify draw config
+        if pg.K_b in self.keyDown:
+            self.grid.switchDrawTileBorder()
+
+        # Camera movement
+        if self.keyboardState[pg.K_UP]:
+            self.grid.moveOffset('u')
+        elif self.keyboardState[pg.K_DOWN]:
+            self.grid.moveOffset('d')
+        if self.keyboardState[pg.K_LEFT]:
+            self.grid.moveOffset('l')
+        elif self.keyboardState[pg.K_RIGHT]:
+            self.grid.moveOffset('r')
+
+        # Change tile status
+        if self.mouseState[0]:
+            tileX = self.mousePos[0] // self.grid.tileSize
+            tileY = self.mousePos[1] // self.grid.tileSize
+            self.grid.set(tileX, tileY, 1)
+
+        elif self.mouseState[2]:
+            tileX = self.mousePos[0] // self.grid.tileSize
+            tileY = self.mousePos[1] // self.grid.tileSize
+            self.grid.set(tileX, tileY, 0)
+
+
+        pg.display.set_caption(f"fps : {self.clock.get_fps():.2f}")
+
+
+    def render(self):
+        """
+        This is the method where all graphic update will be done
+        """
+        # We clean our screen with one color
+        self.win.fill((0, 0, 0))
+
+        self.grid.draw(self.win)
+
+        # We update the drawing.
+        # Before the function call, any changes will be not visible
+        pg.display.update()
+
+
+    def quit(self):
+        """
+        This is the quit method
+        """
+        # Pygame quit
+        pg.quit()
+        sys.exit()
+
+
+print("COMMANDS :")
+print(" - mouse wheel UP -> zoom in grid")
+print(" - mouse wheel DOWN -> zoom out grid")
+print(" - key B -> enable / disable drawing of tiles' border")
+print(" - key UP -> move grid down")
+print(" - key DOWN -> move grid up")
+print(" - key LEFT -> move grid right")
+print(" - key RIGHT -> move grid left")
+
+
+Game().run() # Start game
